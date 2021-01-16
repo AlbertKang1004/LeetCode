0c3e1f9ae5ddb9d3a61f69fc70e3f216e802c187
Albert Kang <albertkang1004@gmail.com>
2021-01-16 16:26:30 -0500

    January 16

--- Leetcode/src/com/leetcode/hashmap/HashSetProblem2.java
+++ Leetcode/src/com/leetcode/hashmap/HashSetProblem2.java
@@ -1,9 +1,30 @@
 package com.leetcode.hashmap;
 
+import java.util.*;
+
 public class HashSetProblem2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String s = "e h";
+		char[] c_arr = s.toCharArray();
+		int max = 0;
+		for (int i = 0; i < c_arr.length; i++) {
+			Set<Character> hash = new HashSet<>();
+			for (int j = i; j < c_arr.length; j++) {
+				if (hash.contains(c_arr[j])) {
+					if (hash.size() > max) {
+						max = hash.size();
+					}
+					break;
+				} else {
+					hash.add(c_arr[j]);
+					if (hash.size() > max) {
+						max = hash.size();
+					}
+				}
+			}
+		}
+		System.out.println(max);
 
 	}
 
